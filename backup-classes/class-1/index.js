306ede0bc08052c386afb665139d114b3a963f59
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-05-10 12:07:22 +0530

    second commit

---
+++ backup-classes/class-1/index.js
@@ -0,0 +1,7 @@
+//react application main file 
+//let ReactDOM = require('react-dom')   ES5 
+import App from './App'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<App />, document.getElementById("root"))
+//what you want to render, where you want to render
