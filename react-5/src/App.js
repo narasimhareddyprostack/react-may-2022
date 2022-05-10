05edeabd7e2b7ac3ffa0ee2ef2b5f7092431a57c
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-05-10 12:06:24 +0530

    first commit

---
+++ react-5/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+
+    return <section><h2>Hello,GM</h2></section>
+}
+export default App
---
+++ react-5/src/index-back.js
@@ -0,0 +1,6 @@
+//react application main file 
+//let ReactDOM = require('react-dom')   ES5 
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(<div><h1>Hello,GM ! MR Rahul Gandhi</h1><h2>Welcome to Wayanad</h2></div>, document.getElementById("root"))
+//what you want to render, where you want to render
