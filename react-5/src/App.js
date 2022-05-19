3cbb3f779d7a7a1c0d3a285ba0095a796ff04b28
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2022-05-19 11:38:52 +0530

    okie

--- react-5/src/App.js
+++ react-5/src/App.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import Navbar from './Navbar/Navbar'
-/* import Hike from './SalaryHike/Hike'
-import Message from './Message/Message'
- 
-import Login from './FormHandling/Login'
-*/
-import SignIn from './FormHandling/SignIn'
-import Registration from './FormHandling/Registration'
-import Employee from './EmployeeData/Employee'
+
 const App = () => {
     return (
         <div>
             <Navbar />
-            <Registration />
 
         </div>
     )
